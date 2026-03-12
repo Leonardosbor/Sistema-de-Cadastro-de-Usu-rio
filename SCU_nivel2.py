9cc88f8ad28f60e2aac2ad280c36b861583f37f8
Leonardosbor <reis8191@gmail.com>
2026-03-12 16:26:22 -0300

    Construção da parte final do Sistema de Cadastro
    Expansão do SCU e criação de mais opções para poder trabalhar com CRUD completo

---
+++ SCU_nivel2.py
@@ -0,0 +1,100 @@
+usuarios = []
+contador = 1
+
+
+while True:
+    print('1 - Cadastrar usuário')
+    print('2 - Listar usuário')
+    print('3 - Buscar Usuário por ID')
+    print('4 - Atualizar usuário')
+    print('5 - Deletar usuário')
+    print('6 - Sair...')
+
+    opcao = input('Digite uma opção:')
+
+    
+    if opcao == '1':
+        nome = input('Digite seu nome: ')
+        email = input('Digite seu e-mail: ')
+        idade = int(input('digite sua idade: '))
+
+        usuario = {
+            'Id': contador,
+            'Nome': nome,
+            'E-mail': email,
+            'Idade': idade
+        }
+
+        usuarios.append(usuario)
+        contador += 1
+
+        print(('Usuário cadastrado com sucesso!'))
+   
+
+    elif opcao == '2':
+        if len(usuarios) == 0:
+            print('Nenhum usuário cadastrado')
+        else:
+            for usuario in usuarios:
+                print(f"ID: {usuario['Id']} | Nome: {usuario['Nome']} | Email: {usuario['E-mail']} | Idade: {usuario['Idade']}")
+    
+
+    elif opcao == '3':
+        id_digitado = int(input('Digite o ID: '))
+
+        encontrado = False
+
+        for usuario in usuarios:
+            if usuario['Id'] == id_digitado:
+                print(usuario)
+                encontrado = True
+        
+        if not encontrado:
+            print('Usuário não encontrado!')
+
+
+    elif opcao == '4':
+        id_solicitado = int(input('Digite um ID: '))
+
+        usuario_encontrado = False
+
+        for usuario in usuarios:
+            if usuario['Id'] == id_solicitado:
+                novo_nome = input('Atualize o nome: ')
+                novo_email = input('Atualize o email: ')
+                nova_idade = int(input('Atualize a idade: '))
+
+                usuario['Nome'] = novo_nome
+                usuario['E-mail'] = novo_email
+                usuario['Idade'] = nova_idade
+
+                usuario_encontrado = True
+            
+        if  not usuario_encontrado:
+            print('Usuário não encontrado!') 
+
+    
+    elif opcao == '5':
+        id_requerido = int(input('Digite o ID: '))
+
+        id_encontrado = False
+
+        for usuario in usuarios:
+            if usuario['Id'] == id_requerido:
+                usuarios.remove(usuario)
+                id_encontrado = True
+                break
+        
+        if not id_encontrado:
+            print('ID não encontrado!')
+
+    
+    elif opcao == '6':
+        print('Saindo...')
+        break
+
+    else:
+        print('Opção inválida. Tente novamente com uma opção válida!')
+
+
+
